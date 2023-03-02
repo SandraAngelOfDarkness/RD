d60fdb0eafd968df7ea4ba967b589bfdb5871f2f
Sandra Krrisiunaite <sandrakrisiunaite@gmail.com>
2023-03-02 14:56:13 +0200

    add models and admins

--- rd_royal/royal/admin.py
+++ rd_royal/royal/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from . import models
+from .models import About, Video, Foto, Blog, Contact
 
-# Register your models here.
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'published_date')
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'published_date')
+
+class FotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'published_date')
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date')
+    list_filter = ('created_date', 'update_date')
+    search_fields = ('title', 'content')
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject')
+
+admin.site.register(models.About, AboutAdmin)
+admin.site.register(models.Video, VideoAdmin)
+admin.site.register(models.Foto, FotoAdmin)
+admin.site.register(models.Blog, BlogAdmin)
+admin.site.register(models.Contact, ContactAdmin)
--- rd_royal/royal/models.py
+++ rd_royal/royal/models.py
@@ -1,2 +1,60 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+import uuid
+from django.contrib.auth.models import User
 
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    video_file = models.FileField(upload_to='videos/')
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.video_file
+    
+    class Meta:
+        verbose_name = ("Video")
+
+class Foto(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.FileField(upload_to='photos/')
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image
+    
+    class Meta:
+        verbose_name = ("Foto")
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = ("Blog")
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = ("Contact")
