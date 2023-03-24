1b95a7a1a2a4399b6b81e69229cb6dea93262637
Sandra Krrisiunaite <sandrakrisiunaite@gmail.com>
2023-03-24 08:50:24 +0200

    perkelta media, upd:index, kad veiktu photo

--- rd_royal/rd_royal/urls.py
+++ rd_royal/rd_royal/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include("royal.urls")),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
